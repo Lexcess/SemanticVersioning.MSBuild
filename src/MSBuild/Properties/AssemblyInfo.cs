a9d153fc1fd1f17170429f2d3da414faaad96f99
Lex Mitchell <lexmitchell@live.com>
2016-02-29 10:40:35 +0000

    Updated name, fixed profileValue error related to not specifiying any dependencies see: https://github.com/ericstj/NuGetDocs/blob/ericstj/nuget3samples/NuGet.Docs/Create/nuget3-packages-walkthrough.md

--- src/MSBuild/Properties/AssemblyInfo.cs
+++ src/MSBuild/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lex.SemanticVersioner")]
+[assembly: AssemblyTitle("SemanticVersioning.MSBuild")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lex.SemanticVersioner")]
+[assembly: AssemblyProduct("SemanticVersioning.MSBuild")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
